98194ab40602c830cf139945d883d4156d94e43f
MiniLini <64050185+MiniLini@users.noreply.github.com>
2022-03-21 19:14:29 -0700

    Update 2_clean.sh

--- 2_clean.sh
+++ 2_clean.sh
@@ -3,9 +3,13 @@
 A_L="/var/log/apache2"
 T_L="/var/log/tomcat7"
 
-cat /dev/null > ${A_L}/access.log*
-cat /dev/null > ${A_L}/error.log*
-cat /dev/null > ${T_L}/local_access.log.2022*
+> ${A_L}/access.log
+> ${A_L}/access.log1
+
+> ${A_L}/error.log
+> ${A_L}/error.log1
+
+> ${T_L}/local_access.log.2022*
 
 rm ${A_L}/g.tar
 rm ${T_L}/catalina.out
