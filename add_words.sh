da0f2bf5caba99d4b1baad2cd93e7bcdb0f0da25
MiniLini <64050185+MiniLini@users.noreply.github.com>
2022-03-24 09:03:33 -0700

    Create add_words.sh

---
+++ add_words.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "thispasswordissup3rsecurebecauseitislong_Summer2018\n
+thispasswordissup3rsecurebecauseitislong_il0v3cats!\n
+thispasswordissup3rsecurebecauseitislong_Fall2018\n
+thispasswordissup3rsecurebecauseitislong_\n
+Summer2018\n
+il0v3cats!\n
+Fall2018" >> ./fast-word.txt
